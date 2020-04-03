a9668ce8e3b8fceb1c78c293343b639193e04d79
momen-mohamed <momenmohamed64@gmail.com>
2020-04-03 03:15:02 +0200

    created all the app

---
+++ ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/momenmohamed/Desktop/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/momenmohamed/Desktop/angulaCourse/dicee-flutter"
+export "FLUTTER_TARGET=/Users/momenmohamed/Desktop/angulaCourse/dicee-flutter/lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "OTHER_LDFLAGS=$(inherited) -framework Flutter"
+export "FLUTTER_FRAMEWORK_DIR=/Users/momenmohamed/Desktop/flutter/bin/cache/artifacts/engine/ios"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
+export "TRACK_WIDGET_CREATION=true"
